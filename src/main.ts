cd3ed19da9432d3fe614107c65ec85d8ed0ef5f3
Epsilon-Galaxy <ajaycausewhynot@gmail.com>
2024-10-18 09:51:48 -0700

    styling changes

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
 import "./style.css";
 
-const APP_NAME = "Sticker Surprise!";
-const app = document.querySelector<HTMLDivElement>("#app")!;
-
-document.title = APP_NAME;
+const APP_NAME = "DEMO 2 TITLE";
+const app: HTMLDivElement = document.querySelector<HTMLDivElement>("#app")!;
 app.innerHTML = APP_NAME;
+document.title = APP_NAME;
+
+
+//Creates the title of the web page
+const head = document.createElement("h1");
+head.innerHTML = APP_NAME;
+app.append(head);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+app.append(canvas);
+
+
+
+
+
+
+
